a43f66e4eb16f9e1bd3fb404a378b0734310ed0a
aldiwebprogreming <alldii1956@gmail.com>
2021-04-07 16:52:40 +0700

    mitrans

---
+++ application/controllers/snap.php
@@ -0,0 +1,143 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+
+
+class Snap extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+
+
+	public function __construct()
+    {
+        parent::__construct();
+        $params = array('server_key' => 'SB-Mid-server-i7JOZecjvYWDeOPvVlfsFta9', 'production' => false);
+		$this->load->library('midtrans');
+		$this->midtrans->config($params);
+		$this->load->helper('url');	
+    }
+
+    public function index()
+    {
+    	$this->load->view('checkout_snap');
+    }
+
+    public function token()
+    {
+    	$price = $this->input->post('price');
+    	$name = $this->input->post('name');
+    	$nama_penerima =$this->input->post('nama_penerima');
+    	$alamat_penerima = $this->input->post('alamat_penerima');
+    	$email_penerima =$this->input->post('email_penerima');
+    	$telp_penerima = $this->input->post('telp_penerima');
+		
+		// Required
+		$transaction_details = array(
+		  'order_id' => rand(),
+		  'gross_amount' => $price, // no decimal allowed for creditcard
+		);
+
+		// Optional
+		$item1_details = array(
+		  'id' => 'a1',
+		  'price' => $price,
+		  'quantity' => 1,
+		  'name' => $name
+		);
+
+		// // Optional
+		// $item2_details = array(
+		//   'id' => 'a2',
+		//   'price' => 25000,
+		//   'quantity' => 1,
+		//   'name' => "Orange"
+		// );
+
+		// Optional
+		$item_details = array ($item1_details);
+
+		// Optional
+		// $billing_address = array(
+		//   'first_name'    => $nama_penerima,
+		//   'last_name'     => "",
+		//   'address'       => "Mangga 20",
+		//   'city'          => "Jakarta",
+		//   'postal_code'   => "16602",
+		//   'phone'         => $telp_penerima,
+		//   'country_code'  => 'IDN'
+		// );
+
+		// // Optional
+		// $shipping_address = array(
+		//   'first_name'    => "Obet",
+		//   'last_name'     => "Supriadi",
+		//   'address'       => "Manggis 90",
+		//   'city'          => "Jakarta",
+		//   'postal_code'   => "16601",
+		//   'phone'         => $telp_penerima,
+		//   'country_code'  => 'IDN'
+		// );
+
+		// Optional
+		$customer_details = array(
+		  'first_name'    => $nama_penerima,
+		  'last_name'     => "",
+		  'email'         => $email_penerima,
+		  'phone'         => $telp_penerima,
+		  
+		);
+
+		// Data yang akan dikirim untuk request redirect_url.
+        $credit_card['secure'] = true;
+        //ser save_card true to enable oneclick or 2click
+        //$credit_card['save_card'] = true;
+
+        $time = time();
+        $custom_expiry = array(
+            'start_time' => date("Y-m-d H:i:s O",$time),
+            'unit' => 'minute', 
+            'duration'  => 2
+        );
+        
+        $transaction_data = array(
+            'transaction_details'=> $transaction_details,
+            'item_details'       => $item_details,
+            'customer_details'   => $customer_details,
+            'credit_card'        => $credit_card,
+            'expiry'             => $custom_expiry
+        );
+
+		error_log(json_encode($transaction_data));
+		$snapToken = $this->midtrans->getSnapToken($transaction_data);
+		error_log($snapToken);
+		echo $snapToken;
+    }
+
+    public function finish()
+    {
+    	$result = json_decode($this->input->post('result_data'));
+    	echo 'RESULT <br><pre>';
+    	var_dump($result);
+    	echo '</pre>' ;
+
+    	// array asosiatif
+    	// $result = json_decode($this->input->post('result_data'), true);
+    	// echo $result['status_code'];
+
+    }
+}
+ 
--- application/views/det_product.php
+++ application/views/det_product.php
@@ -11,6 +11,11 @@
 
 <link href="<?= base_url() ?>assets/select2/dist/css/select2.min.css" rel="stylesheet" />
 
+<script type="text/javascript"
+            src="https://app.sandbox.midtrans.com/snap/snap.js"
+            data-client-key="SB-Mid-client-qeSK1YgKEW8qd-ZU"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
 <!-- jQuery library -->
 <script src="<?= base_url() ?>assets/jquery-3.3.1.min.js"></script>
 
@@ -152,7 +157,23 @@
 			   <label>Grand Total</label><label class="float-right" style="font-size: 23px; color: orange;">Rp.<?= $item2['price'] ?></label>
 			   <hr>
 			<?php } ?>
-				<button class="btn btn-primary btn-sm btn-lg btn-block">Checkout</button>
+
+
+	<form id="payment-form" method="post" action="<?=site_url()?>/snap/finish">
+		<?php foreach ($cart as $pay) {
+			
+		} ?>
+      <input type="hidden" name="result_type" id="result-type" value="">
+      <input type="hidden" name="result_data" id="result-data" value="">
+
+      <input type="hidden" name="name" id="name" value="<?= $pay['name'] ?>">
+      <input type="hidden" name="price" id="price" value="<?= $pay['price'] ?>">
+      <input type="hidden" name="nama_penerima" id="nama_penerima" value="<?= $pay['nama_penerima'] ?>">
+      <input type="hidden" name="telp_penerima" id="telp_penerima" value="<?= $pay['telp_penerima'] ?>">
+      <input type="hidden" name="email_penerima" id="email_penerima" value="<?= $pay['email_penerima'] ?>">
+       <input type="hidden" name="alamat_penerima" id="alamat_penerima" value="<?= $item['kel'] ?>,<?= $item['kec'] ?>,<?= $item['kab'] ?>,<?= $item['prov'] ?>">
+      <button id="pay-button" class="btn btn-primary btn-sm btn-lg btn-block">Checkout</button>
+    </form>
 			  </div>
 
 			</div>
@@ -165,5 +186,71 @@
  		font-weight: bold;
  	}
  </style>
+  <script type="text/javascript">
+  
+    $('#pay-button').click(function (event) {
+      event.preventDefault();
+      $(this).attr("disabled", "disabled");
+
+      var name = $("#name").val();
+      var price = $("#price").val();
+      var nama_penerima = $("#nama_penerima").val();
+      var alamat_penerima = $("#alamat_penerima").val();
+      var email_penerima = $("#email_penerima").val();
+      var telp_penerima = $("#telp_penerima").val();
+
+    
+    $.ajax({
+    type : 'POST',
+    url: '<?= base_url()?>snap/token',
+    data :{
+    	price:price,
+    	name:name,
+    	nama_penerima:nama_penerima,
+    	alamat_penerima:alamat_penerima,
+    	email_penerima:email_penerima,
+    	telp_penerima:telp_penerima
+    },
+    cache: false,
+
+      success: function(data) {
+        //location = data;
+
+        console.log('token = '+data);
+        
+        var resultType = document.getElementById('result-type');
+        var resultData = document.getElementById('result-data');
+
+        function changeResult(type,data){
+          $("#result-type").val(type);
+          $("#result-data").val(JSON.stringify(data));
+          //resultType.innerHTML = type;
+          //resultData.innerHTML = JSON.stringify(data);
+        }
+
+        snap.pay(data, {
+          
+          onSuccess: function(result){
+            changeResult('success', result);
+            console.log(result.status_message);
+            console.log(result);
+            $("#payment-form").submit();
+          },
+          onPending: function(result){
+            changeResult('pending', result);
+            console.log(result.status_message);
+            $("#payment-form").submit();
+          },
+          onError: function(result){
+            changeResult('error', result);
+            console.log(result.status_message);
+            $("#payment-form").submit();
+          }
+        });
+      }
+    });
+  });
+
+  </script>
  </body>
  </html>
